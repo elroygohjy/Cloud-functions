4c59f7f15d45086b45a9ebe7700cc5f902549e4b
elroygohjy <E0543508@u.nus.edu>
2021-06-26 15:10:39 +0800

    feat: add scheduleWebScrap cloud function allows user to manually refresh item via http oncall

---
+++ scheduleWebScrap.js
@@ -0,0 +1,86 @@
+const functions = require("firebase-functions");
+const ptr = require('puppeteer')
+const admin = require('firebase-admin');
+const db = admin.firestore();
+let moment = require('moment')
+
+const priceAndNoti = require('./PriceAndNoti')
+
+exports.scheduledWebScrap = functions.runWith({
+    memory: '8GB',
+    timeoutSeconds: 300
+}).https.onCall(async (data, context) => {
+    const browser = await ptr.launch({
+        args: ['--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote',
+            '--disable-gpu'],
+        headless: true,
+        timeout: 0
+    })
+    const id = context.auth.token.email
+    const userSnapShot = await db.collection('users').doc(id).get()
+    const token = await userSnapShot.data().token
+    const itemSnapShot = await db.collection("users/" + id + "/items").get()
+    for (let doc of itemSnapShot.docs) {
+
+        const url = await doc.data().URL
+        const item_id = doc.id
+        const tPrice = await doc.data().TargetPrice
+        let data = {}
+        if (url.includes("shopee")) {
+            const page = await browser.newPage()
+            await page.setRequestInterception(true);
+            await page.on('request', request => {
+                if (request.resourceType() === 'image')
+                    request.abort();
+                else
+                    request.continue();
+            });
+
+            try {
+                await page.goto(url, {waitUntil: "networkidle2"})
+            } catch (e) {
+                data['name'] = 'Broken URL is given, did you copied correctly?'
+                data['price'] = 'Broken URL is given, did you copied correctly?'
+                await db.collection('users').doc(id).collection('items').doc(item_id).update(data)
+            }
+
+            const priceSelector = await page.$('._3e_UQT')
+            const nameSelector = await page.$('.attM6y')
+            //if the website don't have css for name and price, meaning website fully loaded but it is item not found or 404
+            if (nameSelector !== null && priceSelector !== null) {
+                const retrievePrice = await page.evaluate(async () => {
+                    const price_HTML = await document.querySelector('._3e_UQT')
+                    const price = price_HTML.innerHTML
+                    return price
+                })
+
+                const getName = await page.evaluate(() => {
+                    const name_HTML = document.querySelector('.attM6y')
+                    const name = name_HTML.textContent
+                    return name
+                })
+                const floatPrice = await parseFloat(retrievePrice.replace("$", ""))
+                data['name'] = getName
+                data['price'] = retrievePrice
+                data['lastUpdate'] = await moment(new Date()).fromNow()
+                await priceAndNoti.ExpoPushNotification(tPrice, floatPrice, token, false, getName)
+                await db.collection('users').doc(id).collection('items').doc(item_id).update(data)
+
+            } else {
+                data['name'] = 'Broken URL is given, did you copied correctly?'
+                data['price'] = 'Broken URL is given, did you copied correctly?'
+            }
+            await page.close()
+        } else {
+            data = await priceAndNoti.amazonEbayPriceAndName(url, tPrice, token, false)
+            await db.collection('users').doc(id).collection('items').doc(item_id).update(data)
+        }
+    }
+    await browser.close()
+    return {message: "success"}
+})
